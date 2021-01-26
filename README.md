4ee24d625506aa141b5f45c08a60ab1f5a48a39a
Baruch Odem <baruchiro@gmail.com>
2021-01-26 21:53:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # system-config
+
 My configs for workspaces in Ubuntu, Windows and WSL
+
+# Ubuntu
+
+To configure Ubuntu I'm using the [ubuntu-post-install](https://github.com/tprasadtp/ubuntu-post-install/) project, to install the tools I needed.
+
+Also I have some miscs that not supported in that tool.
+
+To run the tool and the other install, run the `./after-effects/run.sh` file.
+
+Configure the `ubuntu-post-install` by editing the `./after-effects/config.yml`.
+
+Configure the rest installs by the documentation inside the `./after-effects/run.sh`.
+
---
+++ after-effects/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# See docs https://github.com/tprasadtp/ubuntu-post-install
+
+cd "$(dirname "$0")"
+
+after_effect_version="v6.3.0"
+
+wget "https://github.com/tprasadtp/ubuntu-post-install/releases/download/${after_effect_version}/after-effects" -O after-effects
+wget "https://github.com/tprasadtp/ubuntu-post-install/releases/download/${after_effect_version}/sha256sums.txt" -O sha256sums.txt
+sha256sum -c sha256sums.txt --strict
+
+chmod +x after-effects
+sudo ./after-effects --config-file config.yml --autopilot
+
+## More installs
+
+# oh-my-bash
+sh -c "$(wget https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+
+# JetBrainsMono NF
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+sudo unzip JetBrainsMono.zip -d /usr/share/fonts && fc-cache -f -v || echo "########## ERROR JetBrainsMono ###########"
+# In VSCode you may restart the computer, then set the font to 'JetBrainsMono NF', 'Courier New', monospace
+# Also set "editor.fontLigatures": true
+
+
